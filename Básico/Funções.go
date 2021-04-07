c698dbda4de9c534b6e8926508c1f5960e1f6af4
Arthur Jesus <72893926+ArthurwJesus@users.noreply.github.com>
2021-04-07 14:49:39 -0300

    Update Funções.go

--- Básico/Funções.go
+++ Básico/Funções.go
@@ -1,7 +1,7 @@
 package main
 
 import "fmt"
-import "C:/Users/arthu/OneDrive/Área de Trabalho/Go" // aqui é pra puxar uma biblioteca sua
+import "C:/Users/arthu/OneDrive/Área de Trabalho/Go" // Assim se puxa alguma biblitoeca sua ou algum arquivo com funções, precisa adicionar o caminho todo
 
 
 //somando valores

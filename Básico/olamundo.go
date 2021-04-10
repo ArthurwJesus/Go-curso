42c580fd7f6f4da1581d6459537d0dd853668128
Arthur Jesus <72893926+ArthurwJesus@users.noreply.github.com>
2021-04-10 20:05:17 -0300

    Delete Básico directory

--- Básico/olamundo.go
+++
@@ -1,8 +0,0 @@
-// Helo world
-package main //pacote main 
-
-import "fmt" //importando a biblioteca fmt
-
-func main() { //iniciando a função main
-	fmt.Println("Olá mundo") //Aqui vai "imprimir" na tela um olá mundo
-}

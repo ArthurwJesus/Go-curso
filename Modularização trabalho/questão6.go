995ef87af709aae1b0229c058610ff17a5501d91
Arthur Jesus <72893926+ArthurwJesus@users.noreply.github.com>
2021-04-28 19:11:38 -0300

    Create questão6.go

---
+++ Modularização trabalho/questão6.go
@@ -0,0 +1,13 @@
+//Componentes:Arthur de Jeus,Pedro Giasson e João Paulo
+// Linguagem Go lang
+
+package main
+
+import (
+	"area" //importa o pacote area
+	"fmt"
+)
+
+func main() {
+	fmt.Println(area.Circ(6.0)) //utiliza a função Circ que está dentro do pacote area
+}

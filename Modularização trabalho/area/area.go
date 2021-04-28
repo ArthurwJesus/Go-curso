d2dfbc947478fca066986d8dfb67d352382834a8
Arthur Jesus <72893926+ArthurwJesus@users.noreply.github.com>
2021-04-28 19:10:22 -0300

    Create area.go

---
+++ Modularização trabalho/area/area.go
@@ -0,0 +1,13 @@
+
+package area //cria o pacote area
+
+import "math"
+
+// Pi é definido pela relação entre o perímetro
+// de uma circunferência e seu diâmetro
+const Pi = 3.1416
+
+// Circ calcula a área da cincunferência
+func Circ(raio float64) float64 {
+	return math.Pow(raio, 2) * Pi
+}

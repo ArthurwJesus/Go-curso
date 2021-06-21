ea585363dfb7ef0f7c6621459db6e8ac131df1b8
Arthur Jesus <72893926+ArthurwJesus@users.noreply.github.com>
2021-06-21 12:00:27 -0300

    Add files via upload

---
+++ Básico/defer.go
@@ -0,0 +1,30 @@
+//adiar a execução de uma determinada função/pacote... no codigo
+package main
+
+import "fmt"
+
+func texto1() {
+	fmt.Println("Aqui está o meu texto 1")
+}
+
+func texto2() {
+	fmt.Println("Aqui está o meu texto 2")
+}
+
+func soma(N1, N2 int32) bool {
+	defer fmt.Println("Calculada sua media...") //vai aparecer antes do return sempre
+	fmt.Println("Recebendo dados")
+	soma := (N1 + N2)
+
+	if soma >= 4 {
+		return true
+	}
+
+	return false
+}
+
+func main() {
+	defer texto1() //adiou a chamada da função 1 ate o ultimo momento possivel
+	fmt.Println(soma(4, 5))
+	texto2()
+}
---
+++ Básico/panic-revo.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func recuperacao() {
+	if r := recover(); r != nil { //recover recupera a função impedindo que o mesmo pare do nada e retorna false sempre 1 depois da base
+		fmt.Println("recuperando...")
+	}
+}
+
+func media(n1, n2 float64) bool {
+	defer recuperacao() // depois que fizer tudo se entrar em panic chama a recuperação 2 coisa depois da base
+	media := (n1 + n2) / 2
+
+	if media > 7 {
+		return true
+	} else if media < 7 {
+		return false
+	}
+	panic("A media é 7...SOS") //tem q passar algo pra funcionar
+}
+
+func main() {
+	fmt.Println(media(7, 7))
+	fmt.Println("Acabou..")
+}
